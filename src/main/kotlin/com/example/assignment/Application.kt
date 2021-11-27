af746489e7f1bfca375d75b5c1c7ac6af5b0cb06
tomy-lucky <ase3577@gmail.com>
2021-11-28 00:37:57 +0600

    hystrix dashboard, circuit breaker, kafka

--- src/main/kotlin/com/example/assignment/Application.kt
+++ src/main/kotlin/com/example/assignment/Application.kt
@@ -2,8 +2,16 @@ package com.example.assignment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
+@EnableCircuitBreaker
 class Application
 
 fun main(args: Array<String>) {
